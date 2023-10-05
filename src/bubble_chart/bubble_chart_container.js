ec1981000fbfef5eb3d75cfe0df7a147bf04cc54
andres-rubio-go <rubioandres@google.com>
2023-10-05 19:48:34 +0000

    fix: Changed to a zero comparison

--- src/bubble_chart/bubble_chart_container.js
+++ src/bubble_chart/bubble_chart_container.js
@@ -95,7 +95,7 @@ looker.plugins.visualizations.add({
     // need to supply the container with something new if we fail early and
     // don't make it to the inteded render function.
     // https://looker.atlassian.net/browse/DX-5779
-    if (data.length < 1) {
+    if (data.length === 0) {
       this.addError({
         title: "No Results",
       });
