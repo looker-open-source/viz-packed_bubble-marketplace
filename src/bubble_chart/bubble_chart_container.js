dba5e341ee99abfdef70d77bcd66f457b782d54f
andres-rubio-go <132520982+andres-rubio-go@users.noreply.github.com>
2023-10-05 21:14:51 +0000

    fix: No results message not showing (#5)
    * fix: No results message not showing
    
    * fix: Render using react
    
    * fix: Changed to a zero comparison

--- src/bubble_chart/bubble_chart_container.js
+++ src/bubble_chart/bubble_chart_container.js
@@ -91,6 +91,17 @@ looker.plugins.visualizations.add({
     // Clear any errors from previous updates
     this.clearErrors();
 
+    // Issue identified where viz would not change with table calc filters
+    // need to supply the container with something new if we fail early and
+    // don't make it to the inteded render function.
+    // https://looker.atlassian.net/browse/DX-5779
+    if (data.length === 0) {
+      this.addError({
+        title: "No Results",
+      });
+      return;
+    }
+
     const dimensions = [].concat(
       queryResponse.fields.dimensions,
       queryResponse.fields.table_calculations.filter(
@@ -118,13 +129,6 @@ looker.plugins.visualizations.add({
         title: "Dimensions",
         message: "This chart requires at least 1 dimension.",
       });
-      return;
-    }
-    if (data.length === 0) {
-      this.addError({
-        title: "No Results",
-      });
-      done();
       return;
     }
 
