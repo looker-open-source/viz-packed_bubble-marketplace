bc767cf13d85fd76877cbbac84452b3fef516570
andres-rubio-go <132520982+andres-rubio-go@users.noreply.github.com>
2024-04-17 10:24:34 -0600

    fix: upgrades styled-components to remove @babel/traverse vulnerability (#21)

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ var webpackConfig = {
     contentBase: "./dist",
   },
   output: {
-    filename: "dist/packed_bubble.js",
+    filename: "packed_bubble.js",
     path: path.join(path.resolve(__dirname), "/dist"),
     library: "[name]",
     libraryTarget: "umd",
